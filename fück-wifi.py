874d0c3282f1d1b9e39366c1e892051a22936046
Tanvir Hasan <168272220+tanvirhasan999@users.noreply.github.com>
2024-05-24 18:06:08 +0600

    Update fück-wifi.py

--- fück-wifi.py
+++ fück-wifi.py
@@ -26,19 +26,17 @@ logo = ("""
 \033[38;5;46m╚═╝      ╚═════╝  ╚═════╝╚═╝  ╚═╝      ╚══╝╚══╝ ╚═╝╚═╝     ╚═╝ 
 
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-                <==============Created by Tanvir================>
+<==============Created by Tanvir================>
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 [+] OWNER   : TANVIR-HASAN 
 [+] Facebook : https://www.facebook.com/tanvirhasan444?mibextid=ZbWKwL
 [+] Github   : tanvirhasan999
 [+] Version  : 1.9
-[+] Tool     : Free 
- 
+[+] Tool     : Free  
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 \033[38;5;46m [ Wifi-Hack {√} tanvirhasan999 ] \033[38;5;46m               
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-""")
-print(logo)
+""")print(logo)
 print('''
 \033[1;92mRecoded By Tanvir Hasan
 Facebook:https://www.facebook.com/tanvirhasan444?mibextid=ZbWKwL
